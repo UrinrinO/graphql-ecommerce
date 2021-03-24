da4df3489c3d4fb115c099c1952e5fa3faead54c
Urinrin <urinrinogidiama@gmail.com>
2021-03-25 00:13:52 +0100

    slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './default.scss';
+
+// Components
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
     </div>
   );
 }
 
 export default App;
+
